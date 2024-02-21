28e47d6e300a7f2c09615d8dcb71411ac8d3205a
~coding~ <krubab623@gmail.com>
2024-02-21 20:04:31 +0500

    Updates

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,28 +39,26 @@ class _HomeScreenState extends State<HomeScreen> {
       future: products,
       builder: (context, AsyncSnapshot<List<Product>> snapshot) {
         if (snapshot.hasData && snapshot.data != null) {
-          return Container(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: GridView.builder(
-              itemCount: snapshot.data!.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, crossAxisSpacing: 30, mainAxisSpacing: 30),
-              itemBuilder: (context, index) {
-                return GridViewCard(
-                  product: snapshot.data![index],
-                  index: index,
-                  onPressed: () {
-                    onCardPress(snapshot.data![index]);
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //       builder: (context) => const DetailCardScreen(),
-                    //     ));
-                    // Navigator.pushNamed(context, RoutesName.detailCardScreen);
-                  },
-                );
-              },
-            ),
+          return GridView.builder(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            itemCount: snapshot.data!.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, crossAxisSpacing: 30, mainAxisSpacing: 30),
+            itemBuilder: (context, index) {
+              return GridViewCard(
+                product: snapshot.data![index],
+                index: index,
+                onPressed: () {
+                  onCardPress(snapshot.data![index]);
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //       builder: (context) => const DetailCardScreen(),
+                  //     ));
+                  // Navigator.pushNamed(context, RoutesName.detailCardScreen);
+                },
+              );
+            },
           );
         } else {
           return const Center(child: Loader());
